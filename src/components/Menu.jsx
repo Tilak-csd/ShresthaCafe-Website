7729434604bf403bb36198779ef5c7920b328c9a
Tilak-csd <tilakgubhaju3@gmail.com>
2025-12-16 12:28:00 +0545

    changing the hardcode into array map

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,57 +1,29 @@
 import React from 'react'
+import { menu } from '../data/menu';
 
 export default function Menu() {
+
     return (
         <section id='menu' className="w-full pt-20 pb-10 bg-white flex flex-col items-center justify-center gap-10">
             <div className="max-w-6xl mx-auto grid gap-8 px-4 md:grid-cols-3">
 
                 {/* Card 1 */}
-                <div className="rounded-2xl shadow-lg overflow-hidden hover:shadow-2xl transition">
-                    <img
-                        src="./coffee.avif"
-                        alt="Coffee"
-                        loading='lazy'
-                        className="h-56 w-full object-cover"
-                    />
-                    <div className="p-6 text-center">
-                        <h3 className="text-xl font-semibold mb-2">Artisan Coffee</h3>
-                        <p className="text-gray-600 text-sm">
-                            Handcrafted brews made from carefully sourced premium beans.
-                        </p>
+                {menu.map((items, idx) => {
+                    return <div key={idx} className="rounded-2xl shadow-lg overflow-hidden hover:shadow-2xl transition">
+                        <img
+                            src={items.imageUrl}
+                            alt={items.title}
+                            loading='lazy'
+                            className="h-56 w-full object-cover"
+                        />
+                        <div className="p-6 text-center">
+                            <h3 className="text-xl font-semibold mb-2">{items.title}</h3>
+                            <p className="text-gray-600 text-sm">
+                               {items.description}
+                            </p>
+                        </div>
                     </div>
-                </div>
-
-                {/* Card 2 */}
-                <div className="rounded-2xl shadow-lg overflow-hidden hover:shadow-2xl transition">
-                    <img
-                        src="./cuisine.avif"
-                        alt="Food"
-                        loading='lazy'
-                        className="h-56 w-full object-cover"
-                    />
-                    <div className="p-6 text-center">
-                        <h3 className="text-xl font-semibold mb-2">Fresh Cuisine</h3>
-                        <p className="text-gray-600 text-sm">
-                            Thoughtfully prepared dishes balancing flavor, quality, and comfort.
-                        </p>
-                    </div>
-                </div>
-
-                {/* Card 3 */}
-                <div className="rounded-2xl shadow-lg overflow-hidden hover:shadow-2xl transition">
-                    <img
-                        src="./desert.avif"
-                        alt="Desserts"
-                        loading='lazy'
-                        className="h-56 w-full object-cover"
-                    />
-                    <div className="p-6 text-center">
-                        <h3 className="text-xl font-semibold mb-2">Signature Desserts</h3>
-                        <p className="text-gray-600 text-sm">
-                            Indulgent desserts crafted to perfectly complement every meal.
-                        </p>
-                    </div>
-                </div>
+                })}
 
             </div>
 
---
+++ src/data/menu.js
@@ -0,0 +1,18 @@
+export const menu = [
+    {
+        title: "Artisan Coffee",
+        description: "Handcrafted brews made from carefully sourced premium beans.",
+        imageUrl: "./coffee.avif"
+    },
+    {
+        title: "Fresh Cuisine",
+        description: "Thoughtfully prepared dishes balancing flavor, quality, and comfort.",
+        imageUrl: "./cuisine.avif"
+    },
+    {
+        title: "Signature Desserts",
+        description: "Indulgent desserts crafted to perfectly complement every meal.",
+        imageUrl: "./desert.avif"
+    }
+]
+
